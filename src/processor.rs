ee69c6ee16679f3593b9276a8dfa9d39058cf848
J16N <jishan2001bhattacharya@gmail.com>
2024-11-05 22:20:43 +0530

    Modified processor.

--- src/processor.rs
+++ src/processor.rs
@@ -4,25 +4,20 @@ use crate::Result;
 use std::net::SocketAddr;
 use std::path::PathBuf;
 use tokio::fs;
-use tokio::net::TcpSocket;
-use tokio::process::Command;
-use tokio::sync::mpsc;
-use tokio::sync::oneshot::{channel, Receiver, Sender};
-use tokio::task::JoinHandle;
+use tokio::net::TcpListener;
+use tokio::process::{Child, Command};
+use tokio::sync::mpsc::Receiver;
 
 pub(crate) struct RtpProcessor {
     address: SocketAddr,
     client: Option<TcpClient>,
     dir_init: bool,
-    ffmpeg_process: Option<JoinHandle<()>>,
+    ffmpeg_process: Option<Child>,
     imei: String,
-    rx: Option<Receiver<()>>,
-    tx: Option<Sender<()>>,
 }
 
 impl RtpProcessor {
     pub fn new() -> Self {
-        let (tx, rx) = channel::<()>();
         let address = "127.0.0.1:0".parse().expect("Failed to parse address");
 
         Self {
@@ -31,15 +26,13 @@ impl RtpProcessor {
             dir_init: false,
             ffmpeg_process: None,
             imei: String::new(),
-            rx: Some(rx),
-            tx: Some(tx),
         }
     }
 
-    pub async fn listen(&mut self, mut channel: mpsc::Receiver<RtpPacket>) {
+    pub async fn listen(&mut self, mut channel: Receiver<RtpPacket>) {
         while let Some(packet) = channel.recv().await {
             if let Err(e) = self.process(packet).await {
-                eprintln!("Failed to process packet ({}): {e}", self.imei);
+                eprintln!("Failed to process packet: {e}");
                 break;
             }
         }
@@ -53,19 +46,9 @@ impl RtpProcessor {
             }
         }
 
-        if let Some(handle) = self.ffmpeg_process.take() {
-            let tx = self.tx.take().expect("Transmitter not found");
-
-            match tx.send(()) {
-                Ok(_) => {
-                    if let Err(e) = handle.await {
-                        eprintln!("Failed to wait for ffmpeg process ({}): {e}", self.imei);
-                    }
-                }
-
-                Err(_) => {
-                    handle.abort();
-                }
+        if let Some(mut process) = self.ffmpeg_process.take() {
+            if let Err(e) = process.kill().await {
+                eprintln!("Failed to kill ffmpeg process ({}): {e}", self.imei);
             }
         }
 
@@ -84,13 +67,7 @@ impl RtpProcessor {
             self.init_ffmpeg_process().await?;
 
             if let Some(client) = &mut self.client {
-                match client.connect().await {
-                    Ok(_) => (),
-                    Err(e) => {
-                        eprintln!("Client connection failed ({imei}): {}", client.address);
-                        return Err(e);
-                    }
-                };
+                client.connect().await?;
             }
         }
 
@@ -102,10 +79,10 @@ impl RtpProcessor {
     }
 
     async fn init_address(&mut self) -> Result<()> {
-        let socket = TcpSocket::new_v4()?;
-        socket.set_reuseaddr(true)?;
-        socket.bind(self.address)?;
-        let address = socket.local_addr()?;
+        let listener = TcpListener::bind(self.address)
+            .await
+            .expect("Failed to bind listener");
+        let address = listener.local_addr()?;
         self.address = address;
         Ok(())
     }
@@ -124,27 +101,16 @@ impl RtpProcessor {
 
     async fn init_ffmpeg_process(&mut self) -> Result<()> {
         let arguments = format!(
-            "-hide_banner -loglevel error -re -f h264 -i tcp://{}\\?listen -c copy -strftime 1 \
-            -hls_time 2 -hls_segment_filename {}/streams/%Y-%m-%d_%H-%M-%S.ts \
-            -hls_list_size 5 -hls_flags delete_segments -f hls {}/playlist.m3u8",
+            "-hide_banner -loglevel error -re -f h264 -i tcp://{}\\?listen \
+            -c copy -preset:v fast -strftime 1 -hls_time 6 -hls_segment_filename \
+            {}/streams/%Y-%m-%d_%H-%M-%S.ts -hls_list_size 10 -hls_flags \
+            delete_segments -f hls {}/playlist.m3u8",
             self.address, self.imei, self.imei
         );
 
         let arguments: Vec<&str> = arguments.split(' ').collect();
-        let mut child = Command::new("ffmpeg").args(&arguments).spawn()?;
-        let rx = self.rx.take().expect("Receiver not found");
-
-        let handle = tokio::spawn(async move {
-            tokio::select! {
-                _ = child.wait() => (),
-                _ = rx => {
-                    if let Err(e) = child.kill().await {
-                        eprintln!("Failed to kill ffmpeg process: {e}");
-                    }
-                }
-            };
-        });
-        self.ffmpeg_process = Some(handle);
+        let child = Command::new("ffmpeg").args(&arguments).spawn()?;
+        self.ffmpeg_process = Some(child);
         tokio::time::sleep(tokio::time::Duration::from_secs(1)).await;
 
         Ok(())
