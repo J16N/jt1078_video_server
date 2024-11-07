3da135c6f64c2fff35b72040e156a18e8626c33f
J16N <jishan2001bhattacharya@gmail.com>
2024-11-07 22:06:25 +0530

    Use the new run_tcp_server function.

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,10 @@
-use jt1078_video_server::server::TcpServer;
+use jt1078_video_server::run_tcp_server;
 use jt1078_video_server::server::WebServer;
 
 #[tokio::main]
 async fn main() {
-    let web_server = WebServer::new("0.0.0.0", 8080).expect("Failed to create web server");
-    let mut tcp_server = TcpServer::new("0.0.0.0", 8000);
-
-    let tcp_sever_task = tokio::spawn(async move {
-        tcp_server.run().await;
-    });
+    let web_server = WebServer::new("127.0.0.1", 8080).expect("Failed to create web server");
+    let tcp_sever_task = run_tcp_server("0.0.0.0", 8000);
     let _ = web_server.run().await;
-
-    tcp_sever_task.abort();
+    tcp_sever_task.end().await;
 }
